c37af078ea54265f20e07ed4b35318391bdf76f4
Will Liu <William.Q.Liu@gmail.com>
2017-09-10 21:32:32 -0600

    Airflow works

--- airflow/init_script.sh
+++ airflow/init_script.sh
@@ -3,10 +3,16 @@
 echo "Initializing Airflow"
 
 # Kinda hacky way to check if mysql is ready
-while ! mysql -e "select 1" -uroot -pschmeckle -h 172.20.0.2 -P 3306 --protocol=tcp >/dev/null 2>&1; do
+#while ! mysql -e "select 1" -uroot -pschmeckle -h 172.20.0.2 -P 3306 --protocol=tcp >/dev/null 2>&1; do
+while ! mysql -e "select 1" -uroot -pschmeckle -h 172.20.0.2 -P 3306 >/dev/null 2>&1; do
+
 sleep 1
 echo "Waiting for mysql to start"
 done
+
+# Start Scheduler and Worker - Must be started before webserver when using docker
+airflow scheduler &
+airflow worker &
 
 # Run Airflow Webserver
 echo "Running Airflow Webserver"
