3b03e6b27a5073ca27d2800c230fbd9109524a44
Will Liu <William.Q.Liu@gmail.com>
2017-11-18 09:24:46 -0700

    Add sleep to mysql

--- airflow/init_script.sh
+++ airflow/init_script.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+# Wait for MySQL
+sleep 10
 
 echo "Initializing Airflow"
 
 # Kinda hacky way to check if mysql is ready
 #while ! mysql -e "select 1" -uroot -pschmeckle -h 172.20.0.2 -P 3306 --protocol=tcp >/dev/null 2>&1; do
 while ! mysql -e "select 1" -uroot -pschmeckle -h 172.20.0.2 -P 3306 >/dev/null 2>&1; do
-
-sleep 1
-echo "Waiting for mysql to start"
+  sleep 1
+  echo "Waiting for mysql to start"
 done
 
 # initialize the database
